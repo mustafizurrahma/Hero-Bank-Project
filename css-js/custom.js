95e00cf4df169db29d1f0b0da3bc75a3cbc52c77
mustafizurrahma <58368236+mustafizurrahma@users.noreply.github.com>
2021-01-27 15:02:22 +0600

    Some Code Added

--- css-js/custom.js
+++ css-js/custom.js
@@ -1,4 +1,25 @@
 const loginButton = document.getElementById("login");
-loginButton.addEventListener("click", function() {
-    console.log("O Bondhu tmi ki sunte paw");
+loginButton.addEventListener("click", function () {
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    const bankTransfer = document.getElementById("bank-tranction");
+    bankTransfer.style.display = "block";
+})
+
+
+// Deposit Button Control
+const depositBtn = document.getElementById("addDeposit");
+    depositBtn.addEventListener("click", function(){
+    const depositAmount = document.getElementById("depositAmount").value;
+    //Convert system
+    const depositNumber = parseFloat(depositAmount);
+
+    // current Deposit System In Javascript
+
+    const currentDeposit =  document.getElementById("currentDeposit").innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+    
+    document.getElementById("currentDeposit").innerText = totalDeposit;
+    document.getElementById("depositAmount").value = "";
 })
